6cabbdd9b5371ac08873a6772261d68a23bb937d
Glukozavrik <ploplotroll@gmail.com>
2024-02-16 08:53:12 +0300

    encoder works

--- rail-fence-cipher/rail-fence-cipher.js
+++ rail-fence-cipher/rail-fence-cipher.js
@@ -1,12 +1,25 @@
 //
-// This is only a SKELETON file for the 'Rail Fence Cipher' exercise. It's been provided as a
+// This is only a SKELETON file for the 'str Fence Cipher' exercise. It's been provided as a
 // convenience to get you started writing code faster.
 //
-
-export const encode = () => {
-  throw new Error('Remove this statement and implement this function');
+export const encode = (string, railsCount) => {
+  const rails = Array(railsCount).fill("");
+  let rail = 0;
+  let reverse = false;
+  for (let char in string) {
+    rails[rail] += string[char];
+    if (!reverse){
+      rail++
+    } else if (reverse){
+      rail--
+    }
+    if (rail === railsCount - 1 || rail === 0){
+      reverse = !reverse;
+    } 
+  }
+  return rails.join("");
 };
 
-export const decode = () => {
+export const decode = (str, rails) => {
   throw new Error('Remove this statement and implement this function');
 };
