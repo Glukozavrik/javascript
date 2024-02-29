ca408fc2be2d8b839c13b9fd7e72deaeb9c1d3b1
Glukozavrik <ploplotroll@gmail.com>
2024-02-29 14:01:24 +0300

    validated

--- robot-sim/robot-simulator.js
+++ robot-sim/robot-simulator.js
@@ -3,7 +3,7 @@
 // convenience to get you started writing code faster.
 //
 
-let directions = ["north", "east", "south", "west"]
+const directions = ['north', 'east', 'south', 'west'];
 export class InvalidInputError extends Error {
   constructor(message) {
     super();
@@ -13,96 +13,99 @@ export class InvalidInputError extends Error {
 
 export class Robot {
   bearing = 'north'
+
   coordinates = [0, 0]
 
   get bearing() {
-    return this.bearing
+    return this.bearing;
   }
 
   get coordinates() {
-    return this.coordinates
+    return this.coordinates;
   }
 
   place({ x, y, direction }) {
-   if (directions.includes(direction)){
-    this.bearing = direction;
-    this.coordinates = [x, y]
-   } else {
-    throw new InvalidInputError
-   }
+    if (directions.includes(direction)) {
+      this.bearing = direction;
+      this.coordinates = [x, y];
+    } else {
+      throw new InvalidInputError();
+    }
   }
-  turnRigth(){
-    switch(this.bearing){
-      case "north":
-       this.bearing = "east"
-       break;
 
-      case "east":
-        this.bearing = "south"
+  turnRigth() {
+    switch (this.bearing) {
+      case 'north':
+        this.bearing = 'east';
         break;
 
-      case "south":
-        this.bearing = "west"
+      case 'east':
+        this.bearing = 'south';
         break;
 
-      case "west":
-        this.bearing = "north"
+      case 'south':
+        this.bearing = 'west';
+        break;
+
+      case 'west':
+        this.bearing = 'north';
         break;
     }
   }
 
-  turnLeft(){
-    switch(this.bearing){
-      case "north":
-       this.bearing = "west"
-       break;
-
-      case "east":
-        this.bearing = "north"
+  turnLeft() {
+    switch (this.bearing) {
+      case 'north':
+        this.bearing = 'west';
         break;
 
-      case "south":
-        this.bearing = "east"
+      case 'east':
+        this.bearing = 'north';
         break;
 
-      case "west":
-        this.bearing = "south"
+      case 'south':
+        this.bearing = 'east';
+        break;
+
+      case 'west':
+        this.bearing = 'south';
+        break;
+    }
+  }
+
+  advance() {
+    switch (this.bearing) {
+      case 'north':
+        this.coordinates = [this.coordinates[0], this.coordinates[1] + 1];
+        break;
+
+      case 'east':
+        this.coordinates = [this.coordinates[0] + 1, this.coordinates[1]];
+        break;
+
+      case 'south':
+        this.coordinates = [this.coordinates[0], this.coordinates[1] - 1];
+        break;
+
+      case 'west':
+        this.coordinates = [this.coordinates[0] - 1, this.coordinates[1]];
         break;
     }
   }
 
-  advance(){
-    switch(this.bearing){
-      case "north":
-       this.coordinates = [this.coordinates[0], this.coordinates[1] + 1]
-       break;
-
-      case "east":
-        this.coordinates = [this.coordinates[0] + 1, this.coordinates[1]]
-        break;
-
-      case "south":
-        this.coordinates = [this.coordinates[0], this.coordinates[1] - 1]
-        break;
-
-      case "west":
-        this.coordinates = [this.coordinates[0] - 1, this.coordinates[1]]
-        break;
-    }
-  }
   evaluate(instructions) {
-   instructions.split("").forEach((letter) => {
-    switch(letter){
-      case 'R':
-        this.turnRigth()
-        break;
-      case 'L':
-        this.turnLeft()
-        break;
-      case 'A':
-        this.advance()
-        break;
-    }
-   })
+    instructions.split('').forEach((letter) => {
+      switch (letter) {
+        case 'R':
+          this.turnRigth();
+          break;
+        case 'L':
+          this.turnLeft();
+          break;
+        case 'A':
+          this.advance();
+          break;
+      }
+    });
   }
 }
