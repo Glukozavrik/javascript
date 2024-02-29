cc84b081d94dbe2980234ecb1e84ea4da28cc321
Glukozavrik <ploplotroll@gmail.com>
2024-02-29 13:59:26 +0300

    done

--- robot-sim/robot-simulator.js
+++ robot-sim/robot-simulator.js
@@ -3,6 +3,7 @@
 // convenience to get you started writing code faster.
 //
 
+let directions = ["north", "east", "south", "west"]
 export class InvalidInputError extends Error {
   constructor(message) {
     super();
@@ -11,19 +12,97 @@ export class InvalidInputError extends Error {
 }
 
 export class Robot {
+  bearing = 'north'
+  coordinates = [0, 0]
+
   get bearing() {
-    throw new Error('Remove this statement and implement this function');
+    return this.bearing
   }
 
   get coordinates() {
-    throw new Error('Remove this statement and implement this function');
+    return this.coordinates
   }
 
   place({ x, y, direction }) {
-    throw new Error('Remove this statement and implement this function');
+   if (directions.includes(direction)){
+    this.bearing = direction;
+    this.coordinates = [x, y]
+   } else {
+    throw new InvalidInputError
+   }
+  }
+  turnRigth(){
+    switch(this.bearing){
+      case "north":
+       this.bearing = "east"
+       break;
+
+      case "east":
+        this.bearing = "south"
+        break;
+
+      case "south":
+        this.bearing = "west"
+        break;
+
+      case "west":
+        this.bearing = "north"
+        break;
+    }
   }
 
+  turnLeft(){
+    switch(this.bearing){
+      case "north":
+       this.bearing = "west"
+       break;
+
+      case "east":
+        this.bearing = "north"
+        break;
+
+      case "south":
+        this.bearing = "east"
+        break;
+
+      case "west":
+        this.bearing = "south"
+        break;
+    }
+  }
+
+  advance(){
+    switch(this.bearing){
+      case "north":
+       this.coordinates = [this.coordinates[0], this.coordinates[1] + 1]
+       break;
+
+      case "east":
+        this.coordinates = [this.coordinates[0] + 1, this.coordinates[1]]
+        break;
+
+      case "south":
+        this.coordinates = [this.coordinates[0], this.coordinates[1] - 1]
+        break;
+
+      case "west":
+        this.coordinates = [this.coordinates[0] - 1, this.coordinates[1]]
+        break;
+    }
+  }
   evaluate(instructions) {
-    throw new Error('Remove this statement and implement this function');
+   instructions.split("").forEach((letter) => {
+    switch(letter){
+      case 'R':
+        this.turnRigth()
+        break;
+      case 'L':
+        this.turnLeft()
+        break;
+      case 'A':
+        this.advance()
+        break;
+    }
+   })
   }
 }
