c3f50b36dabc63d6e2773cb7daa738ea30fba372
Glukozavrik <ploplotroll@gmail.com>
2024-03-05 12:26:54 +0300

    правки

--- dominoes/dominoes.js
+++ dominoes/dominoes.js
@@ -3,6 +3,14 @@
 // convenience to get you started writing code faster.
 //
 
-export const chain = () => {
+export const chain = (dominoes) => {
+  let summary = []
+if (dominoes.length === 0) {
+return true
+} else if (dominoes.length === 1) {
+let result = (dominoes[0] === dominoes[1]) ?  true : false
+return result
+} else {
 
+}
 };
