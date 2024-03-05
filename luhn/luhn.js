aac28579bbbcbd9db533ed3a852a4fe4dc59635c
Glukozavrik <ploplotroll@gmail.com>
2024-03-05 09:17:40 +0300

    done

--- luhn/luhn.js
+++ luhn/luhn.js
@@ -4,18 +4,24 @@
 //
 
 export const valid = (numbers) => {
-  numbers = numbers.replace(/\s+/g, '').split('');
+  let spl = []
+  numbers.split("").forEach((num) => {
+    if (num !== " "){
+      spl.push(num)
+    }
+  })
+  console.log(spl)
   const newArr = [];
   let multiNum = 0;
   let counter = 0;
-  if (numbers.length < 2) {
+  if (spl.length < 2) {
     return false;
   }
-  for (let i = numbers.length - 2; i >= 0; i -= 2) {
+  for (let i = spl.length - 2; i >= 0; i -= 2) {
     multiNum = 0;
-    if (numbers[i] !== undefined) {
-      multiNum = numbers[i] * 2;
-      newArr.unshift(Number(numbers[i + 1]));
+    if (!(!spl[i])) {
+      multiNum = spl[i] * 2;
+      newArr.unshift(Number(spl[i + 1]));
       if (multiNum > 9) {
         newArr.unshift(multiNum - 9);
       } else {
