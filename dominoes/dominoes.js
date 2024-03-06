a709b5fcba528f516761a801f2c76a43deeca795
Glukozavrik <ploplotroll@gmail.com>
2024-03-06 10:42:35 +0300

    v0.1

--- dominoes/dominoes.js
+++ dominoes/dominoes.js
@@ -4,13 +4,36 @@
 //
 
 export const chain = (dominoes) => {
-  let summary = []
-if (dominoes.length === 0) {
-return true
-} else if (dominoes.length === 1) {
-let result = (dominoes[0] === dominoes[1]) ?  true : false
-return result
-} else {
-
-}
+  let chained = [];
+  if (dominoes.length === 0) {
+    return dominoes;
+  } else if (dominoes.length === 1) {
+    return dominoes[0][0] === dominoes[0][1] ? dominoes : null;
+  } else {
+    chained.push(dominoes[dominoes.length - 1]);
+    dominoes.pop();
+    for (let i = 0; i <= dominoes.length; i += 1) {
+      dominoes.forEach((rock, index) => {
+        if (rock[0] === chained[chained.length - 1][1]) {
+          chained.push(rock);
+          dominoes.splice(index, 1);
+        } else if (rock[1] === chained[0][0]) {
+          chained.unshift(rock);
+          dominoes.splice(index, 1);
+        } else if (rock[1] === chained[chained.length - 1][1]) {
+          chained.push(rock.reverse());
+          dominoes.splice(index, 1);
+        } else if (rock[0] === chained[0][0]) {
+          chained.unshift(rock.reverse());
+          dominoes.splice(index, 1);
+        }
+      });
+    }
+  }
+  console.log(chained);
+  let chain =
+    dominoes.length === 0
+      ? chained[0][0] === chained[chained.length - 1][1]
+      : null;
+  return chain;
 };
