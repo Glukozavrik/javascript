e1cb5d196a019edc946938e3089f0a280d633a09
Glukozavrik <ploplotroll@gmail.com>
2024-03-09 17:06:05 +0300

    done

--- allergies/allergies.js
+++ allergies/allergies.js
@@ -2,17 +2,37 @@
 // This is only a SKELETON file for the 'Allergies' exercise. It's been provided as a
 // convenience to get you started writing code faster.
 //
-
+let allergens = [
+  'eggs',
+  'peanuts',
+  'shellfish',
+  'strawberries',
+  'tomatoes',
+  'chocolate',
+  'pollen',
+  'cats',
+];
+// let allergic = [];
 export class Allergies {
-  constructor() {
-    throw new Error('Remove this statement and implement this function');
+  constructor(input) {
+    this.numb = input;
+    this.allergic = []
   }
 
   list() {
-    throw new Error('Remove this statement and implement this function');
+    this.allergic.length = 0;
+    for (let i = 8; i >= 0; i -= 1) {
+     if (this.numb >= Math.pow(2, i)) {
+        this.numb -= Math.pow(2, i);
+        this.allergic.unshift(allergens[i]);
+    }
+    
   }
+  return this.allergic;
+}
 
-  allergicTo() {
-    throw new Error('Remove this statement and implement this function');
+  allergicTo(allergen) {
+    this.list()
+    return this.allergic.includes(allergen);
   }
 }
