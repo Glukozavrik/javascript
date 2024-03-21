984421bd7436bc3cfa5cbde3b59b7d8eccd1b18f
Glukozavrik <ploplotroll@gmail.com>
2024-03-21 18:01:00 +0300

    done

--- knapsack/knapsack.js
+++ knapsack/knapsack.js
@@ -1,8 +1,27 @@
 //
-// This is only a SKELETON file for the 'Knapsack' exercise. It's been provided as a
+// This is only a SKELETON file for the 'Knapsack' exercise. It's been proitemvalueded as a
 // convenience to get you started writing code faster.
 //
 
 export const knapsack = (maximumWeight, items) => {
-  throw new Error('Remove this statement and implement this function');
+  console.log(items);
+  let newArr = [];
+  newArr[0] = Array(maximumWeight + 1).fill(0);
+  for (let i = 1; i <= items.length; i += 1) {
+    newArr[i] = Array(maximumWeight + 1);
+    let item = items[i - 1];
+    let itemWeight = item.weight;
+    let itemValue = item.value;
+    for (let j = 0; j <= maximumWeight; j += 1) {
+      newArr[i][j] =
+        itemWeight > j
+          ? newArr[i - 1][j]
+          : Math.max(
+              newArr[i - 1][j],
+              itemValue + newArr[i - 1][j - itemWeight],
+            );
+    }
+  }
+  console.log(newArr);
+  return newArr[items.length][maximumWeight];
 };
